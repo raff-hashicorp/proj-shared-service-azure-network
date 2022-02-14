9b63566a4a100fed671b91bc77a422848975b4fb
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-02-14 14:35:19 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+
+  required_version = ">=1.1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
